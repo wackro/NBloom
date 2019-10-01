699157e534b538952a8e801b31e2f006c2c61baa
Joe Griffin <wackro@gmail.com>
2019-10-01 19:16:14 +0100

    Remove extraneous lines

--- NBloom/AssemblyInfo.cs
+++ NBloom/AssemblyInfo.cs
@@ -1,12 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("NBloom.Test")]
-
-namespace NBloom
-{
-    internal class AssemblyInfo
-    {
-    }
-}
