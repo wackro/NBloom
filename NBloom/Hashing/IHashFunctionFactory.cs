b6248326f2d34aa4ab1ea1e1579e7ef51f32a153
Joe Griffin <wackro@gmail.com>
2019-10-08 14:28:30 +0100

    Add hash function factory

---
+++ NBloom/Hashing/IHashFunctionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NBloom.Hashing
+{
+    public interface IHashFunctionFactory<T>
+    {
+        IEnumerable<IHashFunction<T>> GenerateHashFunctions(uint quantity);
+    }
+}
