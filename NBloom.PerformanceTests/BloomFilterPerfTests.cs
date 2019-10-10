daa3f911f20470ef557dfb484897ea4bbf1a2fd2
Joe Griffin <wackro@gmail.com>
2019-10-10 17:50:42 +0100

    Fix perf tests

--- NBloom.PerformanceTests/BloomFilterPerfTests.cs
+++ NBloom.PerformanceTests/BloomFilterPerfTests.cs
@@ -9,69 +9,31 @@ namespace NBloom.PerformanceTests
 {
     class BloomFilterPerfTests
     {
-        static Stopwatch stopwatch;
-        static BoolArrayBloomFilter<int> b;
-        static CompactBloomFilter<int> b2;
-
-        static void Output(object o) => Console.WriteLine(o.ToString());
-
-        static int[] testInputs;
-
-        static BloomFilterPerfTests()
-        {
-            stopwatch = new Stopwatch();
-            b = new BoolArrayBloomFilter<int>(2000000000, 0.0001f, x => BitConverter.GetBytes(x));
-            b2 = new CompactBloomFilter<int>(2000000000, 0.0001f, x => BitConverter.GetBytes(x));
-
-            testInputs = new int[100000];
-        }
+        static Stopwatch _stopwatch = new Stopwatch();
 
         static void Main(string[] args)
         {
-            foreach(var i in Enumerable.Range(0, 100000))
+            var b = new BoolArrayBloomFilter<int>(200000, 0.0001f, x => BitConverter.GetBytes(x));
+            var b2 = new CompactBloomFilter<int>(200000, 0.0001f, x => BitConverter.GetBytes(x));
+            void Output(object o) => Console.WriteLine(o.ToString());
+            int[] testInputs = new int[200000];
+
+            foreach (var i in Enumerable.Range(0, 200000))
             {
                 testInputs[i] = i;
             }
 
             // 700
-            Output(MeasureTime(() =>
-            {
-                b.Add(testInputs);
-            },
-            () =>
-            {
-                b.Clear();
-            }, 100, 5));
+            Output(MeasureTime(() => b.Add(testInputs), () => b.Clear(), 100, 5));
 
             // 1300
-            Output(MeasureTime(() =>
-            {
-                b2.Add(testInputs);
-            },
-            () =>
-            {
-                b2.Clear();
-            }, 100, 5));
+            Output(MeasureTime(() =>b2.Add(testInputs), () =>  b2.Clear(), 100, 5));
 
             // 2.2
-            Output(MeasureTime(() =>
-            {
-                b.Contains(1);
-            },
-            () =>
-            {
-                b.Clear();
-            }, 10000, 5));
+            Output(MeasureTime(() => b.Contains(1), () => b.Clear(), 10000, 5));
 
             // 1.4
-            Output(MeasureTime(() =>
-            {
-                b2.Contains(1);
-            },
-            () =>
-            {
-                b2.Clear();
-            }, 10000, 5));
+            Output(MeasureTime(() => b2.Contains(1), () => b2.Clear(), 10000, 5));
 
             Console.ReadLine();
         }
@@ -82,15 +44,15 @@ namespace NBloom.PerformanceTests
 
             for (var r = 0; r < runs; r++)
             {
-                stopwatch.Start();
+                _stopwatch.Start();
 
                 for (var i = 0; i < iterations; i++)
                 {
                     a();
                 }
 
-                times.Add(stopwatch.ElapsedMilliseconds);
-                stopwatch.Reset();
+                times.Add(_stopwatch.ElapsedMilliseconds);
+                _stopwatch.Reset();
 
                 cleanup();
             }
